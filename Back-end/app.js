d230848a97432de9a7c978f5103898325f4c6d31
ltmervin123 <ltmervin123@gmail.com>
2024-12-06 11:57:27 +0800

    Change Back_End_Port to Port

--- Back-end/app.js
+++ Back-end/app.js
@@ -43,7 +43,7 @@ app.use("/api/user/auth", user);
 app.use(errorHandlerMiddleware);
 
 // Server port
-const PORT = process.env.BACK_END_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 //Start the server
 const startServer = async () => {
